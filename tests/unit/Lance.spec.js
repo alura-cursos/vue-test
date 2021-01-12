70f2d4cbebaaf23100af2560d9e395b1f9546439
Vinicios Neves <vinicios_neves@hotmail.com>
2021-01-12 18:34:45 -0300

    fim video 2.1

--- tests/unit/Lance.spec.js
+++ tests/unit/Lance.spec.js
@@ -3,5 +3,7 @@ import { mount } from '@vue/test-utils'
 
 test('não aceita lance com valor menor do que 0', () => {
     const wrapper = mount(Lance)
-    expect(wrapper).toBeTruthy()
+    const input = wrapper.find('input')
+    input.setValue(-100)
+    expect(input).toBeTruthy()
 })
