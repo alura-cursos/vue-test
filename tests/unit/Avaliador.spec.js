8e6160b36c745dacac2449f593fa085a5557ad90
Vinicios Neves <vinicios_neves@hotmail.com>
2021-01-12 20:05:22 -0300

    fim video 5.2

---
+++ tests/unit/Avaliador.spec.js
@@ -0,0 +1,33 @@
+import Avaliador from '@/views/Avaliador'
+import { mount, RouterLinkStub } from '@vue/test-utils'
+import { getLeiloes } from '@/http'
+import flushPromises from 'flush-promises'
+
+jest.mock('@/http')
+
+const leiloes = [
+  {
+    produto: 'Livro da Casa do Código',
+    lanceInicial: 50,
+    descricao: 'Livro sobre VueJS'
+  },
+  {
+    produto: 'Livro da Casa do Código',
+    lanceInicial: 50,
+    descricao: 'Livro sobre Teste Unitário'
+  }
+]
+
+describe('Um Avaliador que se conecta com a API', () => {
+  test('mostra todos os leilões retornados pela API', async () => {
+    getLeiloes.mockResolvedValueOnce(leiloes)
+    const wrapper = mount(Avaliador, {
+      stubs: {
+        RouterLink: RouterLinkStub
+      }
+    })
+    await flushPromises()
+    const totalLeiloesExibidos = wrapper.findAll('.leilao').length
+    expect(totalLeiloesExibidos).toBe(leiloes.length)
+  })
+})
