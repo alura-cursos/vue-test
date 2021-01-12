3f8ad200e0d4ba7bd62ebc099cdf7c55db25455d
Vinicios Neves <vinicios_neves@hotmail.com>
2021-01-12 19:54:52 -0300

    fim video 4.3

--- tests/unit/Leiloeiro.spec.js
+++ tests/unit/Leiloeiro.spec.js
@@ -1,7 +1,7 @@
 import Leiloeiro from '@/views/Leiloeiro'
 import { mount } from '@vue/test-utils'
-
 import { getLeilao, getLances } from '@/http'
+import flushPromises from 'flush-promises'
 
 jest.mock('@/http')
 
@@ -13,19 +13,15 @@ const leilao = {
 
 describe('Leiloeiro inicia um leilão que ainda não possui lances', () => {
     test('avisa quando não existem lances', async () => {
-      getLeilao.mockResolvedValueOnce(leilao)
-      getLances.mockResolvedValueOnce([{
-        id: 1,
-        valor: 100,
-        data: '2020-12-01',
-        leilao_id: 1
-      }])
-      const wrapper = mount(Leiloeiro, {
-        propsData: {
-          id: 1
-        }
-      })
-      const alerta = wrapper.find('.alert-dark')
-      expect(alerta.exists()).toBe(true)
+        getLeilao.mockResolvedValueOnce(leilao)
+        getLances.mockResolvedValueOnce([])
+        const wrapper = mount(Leiloeiro, {
+            propsData: {
+                id: 1
+            }
+        })
+        await flushPromises()
+        const alerta = wrapper.find('.alert-dark')
+        expect(alerta.exists()).toBe(true)
     })
 })
